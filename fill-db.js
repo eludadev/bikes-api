bfee8b34551ac356a5fdcda1943bbe2230bbd1fc
Eluda <111eluda111@gmail.com>
2022-08-31 21:49:12 +0100

    Build Bikes REST API GET, PATCH, POST, PUT and DELETE endpoints.

---
+++ fill-db.js
@@ -0,0 +1,61 @@
+import DynamoDb from "cyclic-dynamodb";
+
+import slugify from "slugify";
+import { v4 as uuidv4 } from "uuid";
+import { faker } from "@faker-js/faker";
+
+export const bikeAccessor = () => {
+  const type = faker.vehicle.bicycle();
+
+  const title = type + " " + faker.vehicle.model();
+  const handle = slugify(title).toLocaleLowerCase();
+
+  const minPrice = faker.datatype.number({
+    min: 800,
+    max: 3000,
+  });
+  const maxPrice = faker.datatype.number({
+    min: minPrice,
+    max: minPrice * 2,
+  });
+
+  return {
+    id: uuidv4(),
+    title,
+    handle,
+    productType: type,
+    createdAt: String(faker.datatype.datetime()),
+    description: faker.lorem.paragraph(),
+    vendor: faker.vehicle.manufacturer(),
+    availableForSale: faker.datatype.boolean(),
+    totalInventory: faker.datatype.number({ min: 1, max: 5 }),
+    priceRange: {
+      minPrice: {
+        amount: minPrice,
+        currencyCode: "USD",
+      },
+      maxPrice: {
+        amount: maxPrice,
+        currencyCode: "USD",
+      },
+    },
+  };
+};
+
+async function fillDataBaseWithBikes() {
+  const totalBikes = 25;
+  const randomBikes = Array(totalBikes).fill({}).map(bikeAccessor);
+
+  const db = DynamoDb(process.env.DYNAMO_DB);
+  const bikesCollection = db.collection("bikes");
+
+  let i = 1;
+  for (const bike of randomBikes) {
+    console.log(`Adding bike ${i}/${totalBikes}...`);
+    await bikesCollection.set(bike.id, bike);
+
+    i++;
+  }
+}
+
+fillDataBaseWithBikes();
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import express from "express";
+import { router as bikesRouter } from "./router.js";
+
+import { generateAccessToken } from "./auth.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/bikes", bikesRouter);
+
+// Create new bearer token
+app.post("/api/user", (req, res) => {
+  const username = req.body.username;
+
+  const token = generateAccessToken({ username });
+  res.send({ token });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
